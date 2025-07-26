0ea150375f50921ea114810f1ff27559d8df61a4
idhantwastaken <143957788+idhantwastaken@users.noreply.github.com>
2025-07-26 14:12:52 +0530

    afaegwb4eyth

--- src/main/java/com/attendance/Cryption.java
+++ src/main/java/com/attendance/Cryption.java
@@ -1,7 +1,5 @@
 package com.attendance;
 public class Cryption {
-    // Encrypt using ascii values and a string key
-    // The key is stored in Id.id
     public static String encrypt(String text){
         String encryptedtext = "";
         for (int i = 0; i < text.length(); i++) {
